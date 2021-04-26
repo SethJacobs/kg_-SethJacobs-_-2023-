8750b50faa69c4353bedfff30b7e6d5c9f20568a
Seth Jacobs <54406933+SethJacobs@users.noreply.github.com>
2021-04-26 18:02:29 -0400

    made faster run time by using else if to check each char just once

--- main.go
+++ main.go
@@ -9,32 +9,23 @@ func convert(n string) {
 	for i := 0; i <len(n); i++ {
 		if string(n[i]) == "0" {
 			fmt.Print("Zero")
-		}
-		if string(n[i]) == "1"{
+		} else if string(n[i]) == "1"{
 			fmt.Print("One")
-		}
-		if string(n[i]) == "2" {
+		} else if string(n[i]) == "2" {
 			fmt.Print("Two")
-		}
-		if string(n[i]) == "3" {
+		} else if string(n[i]) == "3" {
 			fmt.Print("Three")
-		}
-		if string(n[i]) == "4" {
+		} else if string(n[i]) == "4" {
 			fmt.Print("Four")
-		}
-		if string(n[i]) == "5" {
+		} else if string(n[i]) == "5" {
 			fmt.Print("Five")
-		}
-		if string(n[i]) == "6" {
+		} else if string(n[i]) == "6" {
 			fmt.Print("Six")
-		}
-		if string(n[i]) == "7" {
+		} else if string(n[i]) == "7" {
 			fmt.Print("Seven")
-		}
-		if string(n[i]) == "8" {
+		} else if string(n[i]) == "8" {
 			fmt.Print("Eight")
-		}
-		if string(n[i]) == "9" {
+		} else if string(n[i]) == "9" {
 			fmt.Print("Nine")
 		}
 	}
